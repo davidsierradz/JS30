4d47e09f0f875ed1bb3c237ffac2c0fc520e2b01
davidsierradz <davidsierradz@gmail.com>
2017-09-03 20:56:56 -0500

    more refactors to Day5

--- Day5/app.js
+++ Day5/app.js
@@ -1,23 +1,14 @@
-HTMLElement.prototype.toggleShowFromChildrens = function () {
-  [...this.children]
-    .filter(child => child.nodeName === 'H3')
-    .map(child => child.classList.toggle('show'));
-};
-
 document.querySelectorAll('.column').forEach((column, index, columns) => {
   column.addEventListener('click', (e) => {
     const columnElement = e.currentTarget;
 
     [...columns]
       .filter(childrenColumn =>
-        childrenColumn.classList.contains('column-clicked') && !Object.is(childrenColumn, columnElement))
+        !Object.is(childrenColumn, columnElement))
       .map((childrenColumn) => {
         childrenColumn.classList.remove('column-clicked');
-        childrenColumn.toggleShowFromChildrens();
       });
 
     columnElement.classList.toggle('column-clicked');
-
-    columnElement.toggleShowFromChildrens();
   });
 });
